7c7570695a5c9c73d68a2f71edc488cd92befe93
emmanuel3690 <emmanuelinnn13690@gmail.com>
2025-10-16 15:09:56 -0700

    feat: switched email system to Resend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,31 +1,21 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
   try {
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, // use SSL
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
-
-    const info = await transporter.sendMail({
-      from: `"Geomancy Solution" <${process.env.EMAIL_USER}>`,
+    const data = await resend.emails.send({
+      from: process.env.EMAIL_FROM,
       to,
       subject,
       html,
     });
 
-    console.log("📧 Email sent:", info.response);
+    console.log("📧 Email sent successfully:", data.id);
   } catch (error) {
     console.error("❌ Email sending failed:", error.message);
     throw new Error("Email could not be sent. Try again later.");
   }
-
-  
 };
 
 export default sendEmail;
